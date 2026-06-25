20ada6a809eaf4f037080858d630f65586c4f5e1
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-06-25 11:42:28 +0530

    Time: 2 ms (18.88%), Space: 8.6 MB (41.25%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char>st;
+        for(auto it: jewels){
+            st.insert(it);
+        }
+        int count =0;
+        for(auto it:stones){
+            if(st.find(it)!= st.end()){
+                count++;
+            }
+        }
+        return count;
+    }
+};
