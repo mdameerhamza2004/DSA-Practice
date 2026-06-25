be5947b230b95079f0d0613bcb0017aa8ec387c0
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-06-25 15:53:45 +0530

    Time: 47 ms (16.79%), Space: 64.1 MB (25.89%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -4,15 +4,19 @@ public:
         vector<int> ans;
         int n = nums.size();
         unordered_set<int> st;
-        for(int i=1; i<=n; i++){
-            st.insert(i);
-        }  
-        for(int x : nums) {
-            st.erase(x);
+        for(auto it: nums){
+            st.insert(it);
         }
-        for(auto it: st){
-            ans.push_back(it);
-        } 
+        for(int i=1; i<=n; i++){
+           if(st.find(i)==st.end()){
+            ans.push_back(i);
+           }
+        }  
+        // for(int x : nums) {
+        //     st.erase(x);
+        // }
+        // for(auto it: st){
+        // } 
         return ans;
     }
 };
