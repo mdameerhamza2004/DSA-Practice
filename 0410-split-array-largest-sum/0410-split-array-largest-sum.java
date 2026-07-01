b7cd6963100a5bd0e283c5dcaa307bdca6a1b237
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-01 22:53:35 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.61%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSolution(int arr[], int k, int maxLength){
+        int arrCount = 1;
+        int arrLength = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arrLength + arr[i] <= maxLength){
+                arrLength += arr[i];
+            }
+            else{
+                arrCount++;
+                arrLength =0;
+                if(arr[i]>maxLength){
+                    return false;
+                }
+                if(arrCount > k){
+                    return false;
+                }
+                else{
+                    arrLength = arrLength + arr[i];
+                }
+            }
+        }
+        return true;
+    }
+    public int splitArray(int[] nums, int k) {
+        int sum =0;
+        for(int i: nums){
+            sum += i;
+        }
+        int str =0;
+        int end = sum;
+        int ans = -1;
+        while(str<=end){
+            int mid = str+(end-str)/2;
+            if(isValidSolution(nums, k, mid)){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                str = mid+1;
+            }
+        }
+        return ans;
+    }
+}
