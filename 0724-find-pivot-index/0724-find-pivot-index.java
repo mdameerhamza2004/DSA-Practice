f502a549468fe0a7c338d3134cfcdc425492aa14
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-07 12:19:34 +0530

    Time: 1 ms (98.63%), Space: 47.5 MB (22.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,16 +1,66 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int lfSum =0;
-        for(int i=0; i<nums.length; i++){
-            int rtSum =0;
-            for(int k=i+1; k<nums.length;k++){
-                rtSum += nums[k];
-            }
-            if(lfSum==rtSum){
+        int numSum = 0;
+        for(int it: nums){
+            numSum+=it;
+        }
+        int lSum=0;
+        for(int i=0; i<nums.length;i++){
+            int rSum = numSum-lSum-nums[i];
+            if(lSum==rSum){
                 return i;
             }
-            lfSum += nums[i];
+            lSum += nums[i];
         }
         return -1;
+
+
+
+        // for(int i=0; i<nums.length; i++){
+        //     int lfSum =0;
+        //     int rtSum =0;
+        //     for(int j=0; j<i;j++){
+        //         lfSum += nums[j];
+        //     }
+        //     for(int k=i+1; k<nums.length;k++){
+        //         rtSum += nums[k];
+        //     }
+        //     if(lfSum == rtSum){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+
+
+        // int lfSum =0;
+        // for(int i=0; i<nums.length; i++){
+        //     lfSum += nums[i];
+        //     int rtSum =0;
+        //     for(int k=i+1; k<nums.length;k++){
+        //         rtSum += nums[k];
+        //     }
+        //     if(lfSum-nums[i]==rtSum){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+
+
+        // int lfSum =0;
+        // for(int i=0; i<nums.length; i++){
+        //     int rtSum =0;
+        //     for(int k=i+1; k<nums.length;k++){
+        //         rtSum += nums[k];
+        //     }
+        //     if(lfSum==rtSum){
+        //         return i;
+        //     }
+        //     lfSum += nums[i];
+        // }
+        // return -1;
     }
 }
