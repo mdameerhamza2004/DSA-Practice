945db49b9c863406431bacc0d99dc2e73ea9e6f4
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-07 12:05:34 +0530

    Time: 225 ms (8.33%), Space: 47.4 MB (40.25%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,17 +1,15 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int lfSum =0;
         for(int i=0; i<nums.length; i++){
-            int lfSum =0;
             int rtSum =0;
-            for(int j=0; j<i;j++){
-                lfSum += nums[j];
-            }
             for(int k=i+1; k<nums.length;k++){
                 rtSum += nums[k];
             }
             if(lfSum==rtSum){
                 return i;
             }
+            lfSum += nums[i];
         }
         return -1;
     }
