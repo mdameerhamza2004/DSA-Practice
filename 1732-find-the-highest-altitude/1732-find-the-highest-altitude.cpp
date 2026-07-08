53cbc12b5602e8b67bc9af5151aec0d3ae06f7ff
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-08 20:55:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (41.72%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        for(int i=1; i<gain.size();i++){
+
+            gain[i] = gain[i]+gain[i-1];
+        }
+        int ans = 0;
+        for(auto it: gain){
+            if(ans<it){
+                ans = it;
+            }
+
+        }
+        return ans;
+    }
+};
