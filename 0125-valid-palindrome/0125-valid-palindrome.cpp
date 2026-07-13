c73fafdf82cbf499b5954d00b7bb6bdc07bc612b
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-13 15:04:28 +0530

    Time: 2 ms (36.23%), Space: 10.1 MB (31.84%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(auto it: s){
+            if((it>='a' and it<='z') or (it>='A' and it<='Z') or (it>= '0' and it<= '9')){
+                ans += tolower(it);
+            }
+        }
+        cout<<ans<<endl;
+        int str =0;
+        int end = ans.size()-1;
+        if(end==0){
+            return true;
+        }
+        while(str<=end){
+            if(ans[str]!=ans[end]){
+                cout<<str<<" "<<end<<endl;
+                cout<<ans[str]<<endl;
+                return false;
+                
+            }
+            str++;
+            end--;
+        }
+        return true;
+    }
+};
