85cd0600bfd6c43128138d88d231bdb3fd620e23
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-16 10:47:07 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -17,22 +17,22 @@ public:
         rev(arr,str,end);
     }
     void reverseString(vector<char>& s) {
-        int str =0;
-        int end = s.size()-1;
-        while(str <= end){
-            // if(s[str] != s[end]){
-            //     s[str] = s[str]^s[end];
-            //     s[end] = s[str]^s[end];
-            //     s[str] = s[str]^s[end];
-            // }
-            char temp = s[str];
-            s[str] = s[end];
-            s[end] = temp;
+        // int str =0;
+        // int end = s.size()-1;
+        // while(str <= end){
+        //     // if(s[str] != s[end]){
+        //     //     s[str] = s[str]^s[end];
+        //     //     s[end] = s[str]^s[end];
+        //     //     s[str] = s[str]^s[end];
+        //     // }
+        //     char temp = s[str];
+        //     s[str] = s[end];
+        //     s[end] = temp;
 
-            str++;
-            end--;
-        }
+        //     str++;
+        //     end--;
+        // }
 
-        // rev(s,0,s.size()-1);
+        rev(s,0,s.size()-1);
     }
 };
