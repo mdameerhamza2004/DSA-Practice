f4caf5abb268840a3cf4a47badd4229e1d69a189
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-16 10:46:40 +0530

    Time: 0 ms (100%), Space: 27.2 MB (81.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,35 +4,35 @@ public:
         if(str>end){
             return;
         }
-        if(arr[str]!=arr[end]){
-            arr[str] = arr[str]^arr[end];
-            arr[end] = arr[str]^arr[end];
-            arr[str] = arr[str]^arr[end];
-        }
-        // char temp = arr[str];
-        // arr[str] = arr[end];
-        // arr[end] = temp;
+        // if(arr[str]!=arr[end]){
+        //     arr[str] = arr[str]^arr[end];
+        //     arr[end] = arr[str]^arr[end];
+        //     arr[str] = arr[str]^arr[end];
+        // }
+        char temp = arr[str];
+        arr[str] = arr[end];
+        arr[end] = temp;
 
         str+=1; end-=1;
         rev(arr,str,end);
     }
     void reverseString(vector<char>& s) {
-        // int str =0;
-        // int end = s.size()-1;
-        // while(str <= end){
-        //     // if(s[str] != s[end]){
-        //     //     s[str] = s[str]^s[end];
-        //     //     s[end] = s[str]^s[end];
-        //     //     s[str] = s[str]^s[end];
-        //     // }
-        //     char temp = s[str];
-        //     s[str] = s[end];
-        //     s[end] = temp;
+        int str =0;
+        int end = s.size()-1;
+        while(str <= end){
+            // if(s[str] != s[end]){
+            //     s[str] = s[str]^s[end];
+            //     s[end] = s[str]^s[end];
+            //     s[str] = s[str]^s[end];
+            // }
+            char temp = s[str];
+            s[str] = s[end];
+            s[end] = temp;
 
-        //     str++;
-        //     end--;
-        // }
+            str++;
+            end--;
+        }
 
-        rev(s,0,s.size()-1);
+        // rev(s,0,s.size()-1);
     }
 };
