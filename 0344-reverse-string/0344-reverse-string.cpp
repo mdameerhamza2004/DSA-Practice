20b603d06ac71d32ed777810018fca8a26ffe431
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-16 10:41:58 +0530

    Time: 0 ms (100%), Space: 27.2 MB (52.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
+    void rev(vector<char>& arr, int str, int end){
+        if(str>end){
+            return;
+        }
+        char temp = arr[str];
+        arr[str] = arr[end];
+        arr[end] = temp;
+        str+=1;
+        end-=1;
+        rev(arr,str,end);
+    }
     void reverseString(vector<char>& s) {
+        // int str =0;
+        // int end = s.size()-1;
+        // while(str <= end){
+        //     // if(s[str] != s[end]){
+        //     //     s[str] = s[str]^s[end];
+        //     //     s[end] = s[str]^s[end];
+        //     //     s[str] = s[str]^s[end];
+        //     // }
+        //     char temp = s[str];
+        //     s[str] = s[end];
+        //     s[end] = temp;
+
+        //     str++;
+        //     end--;
+        // }
+
         int str =0;
         int end = s.size()-1;
-        while(str <= end){
-            // if(s[str] != s[end]){
-            //     s[str] = s[str]^s[end];
-            //     s[end] = s[str]^s[end];
-            //     s[str] = s[str]^s[end];
-            // }
-            char temp = s[str];
-            s[str] = s[end];
-            s[end] = temp;
-
-            str++;
-            end--;
-        }
+        rev(s,str,end);
     }
 };
