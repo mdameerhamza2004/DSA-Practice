9a56d6e3172604a6424c4ee619db9ec5347fda71
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-22 21:53:20 +0530

    Time: 0 ms (100%), Space: 9 MB (38.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,24 +2,46 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(auto it: s){
-            if(it=='(' or it=='{' or it=='[' ){
-                st.push(it);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' or s[i]== '{' or s[i]=='['){
+                st.push(s[i]);
             }
             else{
-                if(st.empty()) return false;
-                char top = st.top();
-                if((top == '(' and it== ')') or (top=='{' and it=='}') or (top=='[' and it==']')){
-                    st.pop();
-                }
+                if(st.empty()) return false; //
+                if( s[i]==')' and st.top()=='(' or
+                    s[i]=='}' and st.top()=='{' or
+                    s[i]==']' and st.top()=='['){
+                            st.pop();
+                        }
                 else{
                     return false;
                 }
             }
-        }
+        }  
         if(st.empty()){
             return true;
         }
         return false;
-    }
+    }      
 };
+        // stack<char>st;
+        // for(auto it: s){
+        //     if(it=='(' or it=='{' or it=='[' ){
+        //         st.push(it);
+        //     }
+        //     else{
+        //         if(st.empty()) return false;
+        //         char top = st.top();
+        //         if((top == '(' and it== ')') or (top=='{' and it=='}') or (top=='[' and it==']')){
+        //             st.pop();
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // if(st.empty()){
+        //     return true;
+        // }
+        // return false;
+    // }
