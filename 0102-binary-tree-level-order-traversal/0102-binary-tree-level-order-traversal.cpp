2078d5628822e1090d1f95df0d4d8e43a9e78d66
MD AMEER HAMZA <150888097+mdameerhamza2004@users.noreply.github.com>
2026-07-28 14:57:41 +0530

    Time: 4 ms (12.86%), Space: 17.2 MB (16.31%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -23,9 +23,15 @@ public:
                 TreeNode* curr = q.front();
                 q.pop();
                 level.push_back(curr->val);
-                if(curr->left!=NULL)
-                q.push(curr->left);
-                if(curr->right !=NULL) q.push(curr->right);
+                if(i%2==0){
+                    if(curr->left!=NULL)  q.push(curr->left);
+                    if(curr->right !=NULL) q.push(curr->right);
+                }
+                else{
+                    if(curr->left!=NULL)  q.push(curr->left);
+                    if(curr->right !=NULL) q.push(curr->right);
+
+                }
             }
             ans.push_back(level);
         }
